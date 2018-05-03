2ea36be509a30f7f234bc9cbefdce8f6b4328975
looiic <loic.jermann@gibmit.ch>
2018-05-03 13:42:48 +0200

    sandbox added

---
+++ sandbox/index.php
@@ -0,0 +1,10 @@
+<?php
+
+$posts[] = array('vorname' => 'loic',
+        'nachname' => 'mmimimi',
+        'strasse' => 'nei du doofian',
+        'plz' => '1234',
+        'ort' => 'bi dir',
+        'email' => '100000@dubisch.grusig');
+  echo json_encode($posts);
+ ?>
